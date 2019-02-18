128f854830871b29e83c6be8fb2f26575ee04f8f
hyeongu.jeon <hyeongu.jeon@nhnent.com>
2019-02-18 12:44:59 +0900

    프로그래머스 위장 -> 풀이

--- src/Spy/SpyQuiz.java
+++ src/Spy/SpyQuiz.java
@@ -1,10 +1,29 @@
 package Spy;
 
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class SpyQuiz {
 
-	public long solution(String clothes[][]) {
+	public int solution(String clothes[][]) {
 
-		return 0;
+		Map<String, Integer> clothGroupCounter = new HashMap<>();
+		for (String[] clothGroup : clothes) {
+			String clothGroupName = clothGroup[1];
+			if (clothGroupCounter.containsKey(clothGroupName)) {
+				clothGroupCounter.put(clothGroupName, (clothGroupCounter.get(clothGroupName) + 1));
+			} else {
+				clothGroupCounter.put(clothGroupName, 1);
+			}
+		}
+
+		int count = 1;
+		Set<String> clothGroupNames = clothGroupCounter.keySet();
+		for (String name : clothGroupNames) {
+			count *= (clothGroupCounter.get(name) + 1); //입지않은 경우를 포함
+		}
+		return count - 1; //모두 벗은 경우를 제외
 	}
 }
--- src/Spy/SpyQuizTest.java
+++ src/Spy/SpyQuizTest.java
@@ -14,7 +14,7 @@ public class SpyQuizTest {
 
 		String[][] clothes1 = new String[][]{{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
 		String[][] clothes2 = new String[][]{{"one", "1"}, {"two", "1"}, {"three", "1"}, {"one", "2"}, {"two", "2"}, {"one", "3"}};
-		assertEquals(quiz.solution(clothes1), 5);
-		assertEquals(quiz.solution(clothes2), 23);
+		assertEquals(5, quiz.solution(clothes1));
+		assertEquals(23, quiz.solution(clothes2));
 	}
 }
